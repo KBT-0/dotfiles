517803a37aa13a68e72ecd6ca2eb73a730d05eb7
Kemal Berhan TAN <kemalberhantan@gmail.com>
2026-05-12 16:36:33 +0300

    Add PowerShell profile (Oh My Posh + PSReadLine)

---
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+# Oh My Posh prompt
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\atomic.omp.json" | Invoke-Expression
+
+# PSReadLine: komut geçmişi öneri listesi
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
